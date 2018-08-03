5d7b3c1ea5b227a1d1b4ca3cf907884b51193403
Barbey Ng <barbeyng@er-10-129-221-71.wireless.umn.edu>
2018-08-02 21:22:33 -0500

    export card

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -2,13 +2,24 @@ import React from "react";
 
 export const Card = () => {
   <div class="card" style={"width: 18rem"}>
-    <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+    <img
+      class="card-img-top"
+      src=".../100px180/?text=Image cap"
+      alt="Card image cap"
+    />
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
     </div>
     <div class="card-body text-right">
-      <a href="#" class="card-link">3 comments</a>
+      <a href="#" class="card-link">
+        3 comments
+      </a>
     </div>
-  </div>
+  </div>;
 };
+
+export default Card;
