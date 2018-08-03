8f2a67bce77aaebe798c3749505cac802312aade
Barbey Ng <barbeyng@er-10-129-221-71.wireless.umn.edu>
2018-08-02 19:55:15 -0500

    pages

--- client/src/Pages/About/about.js
+++ client/src/Pages/About/about.js
@@ -1,67 +1,70 @@
 import React, { Component } from "react";
-import blogApi from '../../Utils/blogApi';
+import blogApi from "../../Utils/blogApi";
 import { Link } from "react-router-dom";
 import { Input, FormBtn } from "../../Components/Form";
 import { List, ListItem } from "../../Components/List";
-import { SaveBtn } from "../../Components/SaveBtn"
+import { SaveBtn } from "../../Components/SaveBtn";
 import { Col, Row, Container } from "../../Components/Grid";
 import { Nav } from "../../Components/Nav";
 import { Jumbotron } from "../../Components/Jumbotron";
 import { Card } from "../../Components/Card";
 
 class About extends Component {
+  // state = {
+  //   blogs: []
+  // };
 
-  state={
-    blogs: []
-  };
+  // componentDidMount() {
+  //   this.loadBlogs();
+  // }
 
-  componentDidMount() {
-    this.loadBlogs();
-  }
-
-  loadBlogs = () => {
-    blogApi.getBlogs()
-      .then(res => this.setState({ blogs: res.data }))
-      .catch(err => console.log(err));
-  };
+  // loadBlogs = () => {
+  //   blogApi
+  //     .getBlogs()
+  //     .then(res => this.setState({ blogs: res.data }))
+  //     .catch(err => console.log(err));
+  // };
 
   render() {
-
     return (
       <div>
-        {/* <Nav /> */}
-        {/* <Jumbotron /> */}
+        <Nav />
         <Container>
           <Row>
-            <Col size="md-12">   
-              <h2>Recent blogs</h2>
+            <Col size="md-12">
+              <h2>ABout us</h2>
             </Col>
           </Row>
           <Row>
-            <Col size="xs-12">
-              {!this.state.blogs.length ? 
-                (
-                  <h1 className="text-center">No recent blogs</h1>
-                ) : 
-                (
-                  this.state.blogs.map(blog => {
-                    return (
-                      <Card
-                        title={blog.title}
-                        content={blog.content}
-                        imageSrc={blog.imageSrc}
-                        createdDt={blog.created_dt}
-                      />
-                    );
-                  })
-                )
-              }
+            <Col size="xs-12 md-5">IMAGE GOES HERE</Col>
+            <Col size="xs-12 md-7">
+              “Just Google it!" is becoming our standard response to unanswered
+              questions in life. However, when we enter key words in a search
+              engine such as Google, we end up with thousands of websites - many
+              of which are barely relevant to what we are looking for or are
+              rife with inaccuracies. Identifying websites with the most
+              accurate information is a critical skill that is necessary for
+              navigating our way in this jungle. When it comes to medical
+              information, inaccurate or irrelevant information could
+              potentially have a major detrimental impact on our well-being.
+              Educational websites only had 50.2% accurate medical information.
+              The majority of the books found by the search engine either
+              provided outdated or irrelevant information, which may have also
+              contributed to the low accuracy rate of educational websites.
+              Blogs and websites of individuals also had very low rates of
+              medical accuracy (25.7% and 30.3%). We created The Real Doctors
+              because of a gap in correct medical information. Instead of
+              placing the burden of discriminating between accurate and
+              inaccurate information on patients, they can come to our website
+              which guarantees that the information is written by board
+              certified doctors. All the information is open to the public, but
+              it can only be written by doctors who are highly trained.
             </Col>
           </Row>
         </Container>
+        <Footer />
       </div>
     );
-    
   }
 }
 
