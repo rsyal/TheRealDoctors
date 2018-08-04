4c68be7c5a9feb39e710d130a2d7b93fc13c455d
Barbey Ng <barbeyng@er-10-129-72-199.wireless.umn.edu>
2018-08-04 12:05:02 -0500

    blogger database in signup

--- client/src/Pages/Signup/signup.js
+++ client/src/Pages/Signup/signup.js
@@ -1,57 +1,113 @@
 import React, { Component } from "react";
-import blogApi from "../../Utils/blogApi";
+import bloggerApi from "../../Utils/bloggerApi";
 // import  Link  from "react-router-dom";
-// import { Input, FormBtn } from "../../Components/Form";
 // import { List, ListItem } from "../../Components/List";
 // import  SaveBtn  from "../../Components/SaveBtn";
 // import  Jumbotron  from "../../Components/Jumbotron";
 // import  Card  from "../../Components/Card";
 import { Col, Row, Container } from "../../Components/Grid";
+import { Input, FormBtn } from "../../Components/Form";
 
 class Signup extends Component {
   state = {
-    blogs: []
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    specialty: "",
+    identification_id: "",
+    created_dt: ""
   };
 
-  componentDidMount() {
-    this.loadBlogs();
-  }
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-  loadBlogs = () => {
-    blogApi
-      .getBlogs()
-      .then(res => this.setState({ blogs: res.data }))
-      .catch(err => console.log(err));
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (
+      this.state.first_name &&
+      this.state.last_name &&
+      this.state.specialty &&
+      this.state.email &&
+      this.state.password
+    ) {
+      bloggerApi
+        .saveBlogger({
+          email: this.state.email,
+          password: this.state.password,
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          specialty: this.state.specialty,
+          identification_id: this.state.identification_id,
+          created_dt: this.state.created_dt
+        })
+        .then(bloggerData => console.log(bloggerData.data))
+        .catch(err => console.log(err));
+    }
   };
 
   render() {
     return (
       <div>
-        {/* <Nav />
-        <Jumbotron /> */}
         <Container>
           <Row>
             <Col size="md-12">
-              <h2>Recent blogs</h2>
+              <h2>Sign up to write for us</h2>
             </Col>
           </Row>
           <Row>
-            <Col size="xs-12">
-              {!this.state.blogs.length ? (
-                <h1 className="text-center">No recent blogs</h1>
-              ) : (
-                this.state.blogs.map(blog => {
-                  return (
-                    "Hello World"
-                    // <Card
-                    //   title={blog.title}
-                    //   content={blog.content}
-                    //   imageSrc={blog.imageSrc}
-                    //   createdDt={blog.created_dt}
-                    // />
-                  );
-                })
-              )}
+            <Col size="md-12">
+              <form>
+                <Input
+                  value={this.state.first_name}
+                  onChange={this.handleInputChange}
+                  name="first_name"
+                  placeholder="First name"
+                />
+                <Input
+                  value={this.state.last_name}
+                  onChange={this.handleInputChange}
+                  name="last_name"
+                  placeholder="Last name"
+                />
+                <Input
+                  value={this.state.specialty}
+                  onChange={this.handleInputChange}
+                  name="specialty"
+                  placeholder="Specialty"
+                />
+                <Input
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  name="email"
+                  placeholder="Google email"
+                />
+                <Input
+                  value={this.state.password}
+                  onChange={this.handleInputChange}
+                  name="password"
+                  type="password"
+                  placeholder="Google password"
+                />
+                <FormBtn
+                  disabled={
+                    !(
+                      this.state.first_name &&
+                      this.state.last_name &&
+                      this.state.specialty &&
+                      this.state.email &&
+                      this.state.password
+                    )
+                  }
+                  onClick={this.handleFormSubmit}
+                >
+                  Submit
+                </FormBtn>
+              </form>
             </Col>
           </Row>
         </Container>
