5e22b1c3a085867ef29af46f4b97c36a6d0d8b03
Barbey Ng <barbeyng@er-10-129-171-247.wireless.umn.edu>
2018-08-09 21:07:32 -0500

    detail image now working and start comments

--- client/src/Pages/Detail/Detail.js
+++ client/src/Pages/Detail/Detail.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import blogApi from "../../Utils/blogApi";
+import commentApi from "../../Utils/commentApi";
 import { Link } from "react-router-dom";
-// import { Input, FormBtn } from "../../Components/Form";
+import { Input, FormBtn } from "../../Components/Form";
 // import { List, ListItem } from "../../Components/List";
 // import  SaveBtn  from "../../Components/SaveBtn";
 // import  Jumbotron  from "../../Components/Jumbotron";
 import { Col, Row, Container } from "../../Components/Grid";
 import Card from "../../Components/Card";
+// import Comment from "../../Components/Comment";
 
 class Detail extends Component {
   state = {
     blog: {}
   };
 
+  // Retrieve all blogs
   componentDidMount() {
     blogApi
       .getBlog(this.props.match.params.id)
@@ -23,12 +26,28 @@ class Detail extends Component {
       .catch(err => console.log(err));
   }
 
+  // Retrieve all comments
+  // componentDidMount() {
+  //   commentApi
+  //     .getComments(this.props.match.params.id)
+  //     .then(res => {
+  //       this.setState({ comment: res.data });
+  //       console.log(res.data);
+  //     })
+  //     .catch(err => console.log(err));
+  // }
+
   render() {
     return (
       <Container>
         <Row>
           <Col size="md-12">
             <h1>{this.state.blog.topic}</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <img src={this.state.blog.imageSrc} />
           </Col>
         </Row>
         <Row>
@@ -43,9 +62,15 @@ class Detail extends Component {
           </Col>
         </Row>
         <Row>
-          <Col size="md-2">
+          <Col size="md-12">
             <Link to="/">← Back to Blogs</Link>
           </Col>
+        </Row>
+        <Row>
+          <h3>Comment below:</h3>
+        </Row>
+        <Row>
+          <Col size="md-12" />
         </Row>
       </Container>
     );
