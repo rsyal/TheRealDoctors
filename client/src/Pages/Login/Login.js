68f0b713688acc806cc5c396f51a1a46f4b52818
Barbey Ng <barbeyng@Barbeys-MacBook-Pro.local>
2018-08-17 20:23:12 -0500

    fixed conflict

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -10,21 +10,16 @@ import "./Login.css";
 // import { fromPrefixLen } from "ip";
 
 class Login extends Component {
-  state = { 
-    isAuthenticated: false, 
-    user: null, 
+  state = {
+    isAuthenticated: false,
+    user: null,
     token: "",
     currentUser: {}
-     };
+  };
 
   logout = () => {
-<<<<<<< HEAD
-		this.setState({isAuthenticated: false, token: '', currentUser: null});
-		this.props.history.push('/')
-=======
-    this.setState({ isAuthenticated: false, token: "", user: null });
+    this.setState({ isAuthenticated: false, token: "", currentUser: null });
     this.props.history.push("/");
->>>>>>> barbey0817
   };
 
   onFailure = error => {
@@ -36,7 +31,7 @@ class Login extends Component {
       [JSON.stringify({ access_token: response.accessToken }, null, 2)],
       { type: "application/json" }
     );
-    console.log('tokenBlob ', tokenBlob);
+    console.log("tokenBlob ", tokenBlob);
 
     const options = {
       method: "POST",
@@ -45,11 +40,10 @@ class Login extends Component {
       cache: "default"
     };
 
-    fetch("http://localhost:3002/api/v1/auth/google", options)
-    .then(r => {
-      console.log("r ", r)
+    fetch("http://localhost:3002/api/v1/auth/google", options).then(r => {
+      console.log("r ", r);
       const token = r.headers.get("x-auth-token");
-      console.log('token ', token);
+      console.log("token ", token);
       r.json().then(currentUser => {
         if (token) {
           this.setState({ isAuthenticated: true, currentUser, token });
@@ -60,15 +54,14 @@ class Login extends Component {
     });
   };
 
-<<<<<<< HEAD
-  setUserInfo = (user) => {
+  setUserInfo = user => {
     // window.sessionStorage.setItem("userDisplayName", user.displayName);
     // window.sessionStorage.setItem("userEmail", user.email);
     // window.sessionStorage.setItem("userId", user.id);
     // window.sessionStorage.setItem("googleId", googleProvider.id);
     // window.sessionStorage.setItem("accessToken", googleProvider.token);
     sessionStorage.setItem("currentUser", JSON.stringify(user));
-  }
+  };
 
   getUserInfo = () => {
     // window.sessionStorage.getItem("userDisplayName");
@@ -76,94 +69,60 @@ class Login extends Component {
     // window.sessionStorage.getItem("userId");
     // window.sessionStorage.getItem("googleId");
     // window.sessionStorage.getItem("accessToken");
-    JSON.parse(sessionStorage.getItem('currentUser'))}
-  
-
-  render() {
-    let content = !!this.state.isAuthenticated ? (
-        <ul className="navbar-nav ml-auto">
-          {/* Welcome {this.state.currentUser.email}  */}
-          <li className="nav-item">
-              <NavLink className="nav-link" to="/About">
-                About
-              </NavLink>
-          </li>
-          <li className="nav-item">
-              <NavLink className="nav-link" to="/Summary">
-                Blogger Summary
-              </NavLink>
-          </li>
-          <li className="nav-item login-wrapper">
-            <GoogleLogout
-              className="google-logout logout-button"
-              buttonText="Logout"
-              onLogoutSuccess={this.logout}
-            />
-          </li>
-        </ul>
-    ) : (
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-              <NavLink className="nav-link" to="/About">
-                About
-              </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/Signup">
-                    Sign Up to Write
-            </NavLink>
-          </li>
-          <li className="nav-item login-wrapper">
-            <GoogleLogin
-              className="login-button"
-              clientId={config.GOOGLE_CLIENT_ID}
-              buttonText="Login"
-              onSuccess={this.googleResponse}
-              onFailure={this.onFailure}
-            />
-          </li>
-        </ul>
-    );
-
-	return (
-		<div className="collapse navbar-collapse" id="navbarColor02">
-			{content}
-		</div>);
-=======
-  setUserInfo = user => {
-    window.sessionStorage.setItem("userName", user.display_name);
-    window.sessionStorage.setItem("userEmail", user.email);
-    window.sessionStorage.setItem("userId", user.id);
-  };
-
-  getUserInfo = () => {
-    window.sessionStorage.getItem("userName");
-    window.sessionStorage.getItem("userEmail");
-    window.sessionStorage.getItem("userId");
+    JSON.parse(sessionStorage.getItem("currentUser"));
   };
 
   render() {
     let content = !!this.state.isAuthenticated ? (
-      <span className="text-light paddingRight-20">
-        Welcome {this.state.user.email}
-        <GoogleLogout
-          className="google-logout"
-          buttonText="Logout"
-          onLogoutSuccess={this.logout}
-        />
-      </span>
+      <ul className="navbar-nav ml-auto">
+        {/* Welcome {this.state.currentUser.email}  */}
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/About">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Summary">
+            Blogger Summary
+          </NavLink>
+        </li>
+        <li className="nav-item login-wrapper">
+          <GoogleLogout
+            className="google-logout logout-button"
+            buttonText="Logout"
+            onLogoutSuccess={this.logout}
+          />
+        </li>
+      </ul>
     ) : (
-      <GoogleLogin
-        className="google-login"
-        clientId={config.GOOGLE_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={this.googleResponse}
-        onFailure={this.onFailure}
-      />
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/About">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/Signup">
+            Sign Up to Write
+          </NavLink>
+        </li>
+        <li className="nav-item login-wrapper">
+          <GoogleLogin
+            className="login-button"
+            clientId={config.GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.googleResponse}
+            onFailure={this.onFailure}
+          />
+        </li>
+      </ul>
     );
 
-    return <div>{content}</div>;
->>>>>>> barbey0817
+    return (
+      <div className="collapse navbar-collapse" id="navbarColor02">
+        {content}
+      </div>
+    );
   }
 }
 
