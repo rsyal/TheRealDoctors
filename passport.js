2a73d6fd2f102af9b71714f08246b52cf4074037
Steph Japs <stephjaps@gmail.com>
2018-08-21 19:16:51 -0500

    using env

--- passport.js
+++ passport.js
@@ -5,12 +5,13 @@ const passport = require('passport');
 //const TwitterTokenStrategy = require('passport-twitter-token');
 //const FacebookTokenStrategy = require('passport-facebook-token');
 const GoogleTokenStrategy = require('passport-google-token').Strategy;
-const config = require('./config');
+// const config = require('./config');
 
 module.exports = function () {
     passport.use(new GoogleTokenStrategy({
-            clientID: config.googleAuth.clientID,
-            clientSecret: config.googleAuth.clientSecret
+            clientID: process.env.clientID,
+            clientSecret: process.env.clientSecret
+            // callbackURL: "http://localhost:3002/auth/google/callback"
         },
         function (accessToken, refreshToken, profile, done) {
             User.upsertGoogleUser(accessToken, refreshToken, profile, function(err, user) {
